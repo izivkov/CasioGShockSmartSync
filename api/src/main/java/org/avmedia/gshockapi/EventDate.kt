5ea84ed35e47ee87066d74f05941512a017b7d59
Ivo Zivkov <izivkov@gmail.com>
2024-05-22 12:16:38 -0400

    Enter or paste your release notes for en-US here

--- api/src/main/java/org/avmedia/gshockapi/EventDate.kt
+++
@@ -1,9 +0,0 @@
-package org.avmedia.gshockapi
-
-import java.time.Month
-
-class EventDate(var year: Int, val month: Month, val day: Int) {
-    fun equals(eventDate: EventDate): Boolean {
-        return eventDate.year == year && eventDate.month == month && eventDate.day == day
-    }
-}
