82fbd53b3b9d3be36db0f8414b437be1f69d8b9d
Just Me <izivkov@gmail.com>
2024-12-31 14:51:13 -0500

    Update to new translation lib

--- app/src/main/java/org/avmedia/gshockGoogleSync/GShockApplication.kt
+++ app/src/main/java/org/avmedia/gshockGoogleSync/GShockApplication.kt
@@ -55,7 +55,7 @@ class GShockApplication : Application() {
         ).getGShockRepository()
 
         deviceManager
-        translateApi.setSaveMode(true)
+        translateApi.setSafeMode(true)
         createAppEventsSubscription()
     }
 
