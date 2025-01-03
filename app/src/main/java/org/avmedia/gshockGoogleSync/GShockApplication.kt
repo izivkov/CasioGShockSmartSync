0c2167651390626e4eb87943a6e5622c7d67de89
Just Me <izivkov@gmail.com>
2025-01-03 09:35:29 -0500

    Handling auto-translation exceptions

--- app/src/main/java/org/avmedia/gshockGoogleSync/GShockApplication.kt
+++ app/src/main/java/org/avmedia/gshockGoogleSync/GShockApplication.kt
@@ -55,7 +55,7 @@ class GShockApplication : Application() {
         ).getGShockRepository()
 
         deviceManager
-        translateApi.setSafeMode(false)
+        translateApi.setSafeMode(true)
         createAppEventsSubscription()
     }
 
