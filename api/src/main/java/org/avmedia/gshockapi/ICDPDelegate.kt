c0d86a9387afeaf7d4b40f0349bf157171a3c717
Ivo Zivkov <izivkov@gmail.com>
2026-07-17 20:47:48 -0400

    packed stratchpad data

--- api/src/main/java/org/avmedia/gshockapi/ICDPDelegate.kt
+++
@@ -1,8 +0,0 @@
-package org.avmedia.gshockapi
-
-import android.content.IntentSender
-
-interface ICDPDelegate {
-    fun onChooserReady(chooserLauncher: IntentSender)
-    fun onError(error: String)
-}
